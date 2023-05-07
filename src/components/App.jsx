711f930bf1f49307a180c229e32ada53f31ded16
Serhii-Buha <serhii.buga@gmail.com>
2023-05-07 13:23:50 +0300

    render by condition ContactList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,7 @@ import { ContactList } from './ContactList/ContactList';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -62,6 +57,7 @@ export class App extends Component {
 
   render() {
     const filterContacts = this.getVisibleFilter();
+    const { contacts } = this.state;
 
     return (
       <Container>
@@ -71,10 +67,12 @@ export class App extends Component {
         <h2>Contacts</h2>
         <Filter value={this.state.filter} onChange={this.changeFilter} />
 
-        <ContactList
-          contacts={filterContacts}
-          onDeleteContact={this.deleteContact}
-        />
+        {contacts.length > 0 && (
+          <ContactList
+            contacts={filterContacts}
+            onDeleteContact={this.deleteContact}
+          />
+        )}
       </Container>
     );
   }
