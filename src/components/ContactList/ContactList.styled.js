a576584645a4ea9b5f382d199b9f1b2bc116bb09
Serhii-Buha <serhii.buga@gmail.com>
2023-05-07 10:29:35 +0300

    add component ContactList, list contacts, button delete  and styles

---
+++ src/components/ContactList/ContactList.styled.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const SectionTitle = styled.h2`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin-bottom: 10px;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+
+  padding: 0;
+
+  list-style: none;
+`;
+
+export const Item = styled.li`
+  display: flex;
+
+  margin: 0;
+  padding: 0;
+`;
+
+// import styled from '@emotion/styled';
+
+// export const List = styled.ul`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+
+//   list-style: none;
+// `;
+
+// export const Item = styled.li`
+//   display: flex;
+//   flex-direction: row;
+//   justify-content: flex-start;
+//   align-items: center;
+
+//   width: 320px;
+//   padding: 5px;
+//   margin-bottom: 10px;
+
+//   -webkit-box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12),
+//     0px 1px 1px rgba(0, 0, 0, 0.14), 0px 2px 1px rgba(0, 0, 0, 0.2);
+//   box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
+//     0px 2px 1px rgba(0, 0, 0, 0.2);
+//   border-radius: 10px;
+
+//   background-color: #f3f6f9;
+// `;
