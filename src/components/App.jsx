474a60e705484bea7a430244a6f973235badf3a3
Serhii-Buha <serhii.buga@gmail.com>
2023-05-08 13:00:13 +0300

    add comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,43 +15,46 @@ export class App extends Component {
     console.log(data);
 
     const { contacts } = this.state;
-    const isExist = contacts.some(contact => contact.name === data.name);
+    const isExist = contacts.some(contact => contact.name === data.name); // ищет первое совпадение по name
 
     if (isExist) {
-      alert(`${data.name} is already in contacts.`);
+      alert(`${data.name} is already in contacts.`); // если true нашел совпадение  по name пишет ошибку и возврат
       return;
     }
 
     const newContact = {
-      id: nanoid(),
-      name: data.name,
-      number: data.number,
+      // форма добавления нового контакта
+      id: nanoid(), // генерация уникального id
+      name: data.name, // это имя нового контакта, которое было введено пользователем в форму
+      number: data.number, // это номер телефона нового контакта, который также был введен пользователем в форму.
     };
 
     this.setState(prevState => ({
-      contacts: [newContact, ...prevState.contacts],
+      contacts: [newContact, ...prevState.contacts], // Выражение [newContact, ...prevState.contacts] создает новый массив контактов, в котором первым элементом является новый контакт (newContact), а остальные элементы берутся из предыдущего состояния массива контактов (prevState.contacts).
     }));
   };
 
   changeFilter = e => {
     this.setState({
-      filter: e.currentTarget.value,
+      filter: e.currentTarget.value, //извлекается значение, введенное пользователем в поле фильтрации и перезаписывается state.filter
     });
   };
 
+  // фильтрует контакты на основе текущего значения фильтра.
   getVisibleFilter = () => {
     const { contacts, filter } = this.state;
 
-    const normalizeFilter = filter.toLowerCase();
+    const normalizeFilter = filter.toLowerCase(); //  устанавливается в значение filter, полученное из текущего состояния фильтра и переведенное в нижний регистр с помощью метода
 
-    return contacts.filter(todo =>
-      todo.name.toLowerCase().includes(normalizeFilter)
+    return contacts.filter(
+      todo => todo.name.toLowerCase().includes(normalizeFilter) // для получения нового массива контактов, который содержит только те контакты, чьи имена (также переведенные в нижний регистр) содержат подстроку, заданную в фильтре
     );
   };
 
+  //  это функция, которая удаляет контакт из списка контактов, на основе переданного contactId.
   deleteContact = contactId => {
     this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId), // filter(), который создает новый массив контактов, содержащий только те контакты, у которых id не равен переданному contactId. Таким образом, из списка контактов удаляется контакт с указанным id.
     }));
   };
 
